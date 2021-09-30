07e12a15768be4120a8b8de78ac9c0a363e37b37
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2021-09-29 20:43:31 -0400

    update syntax per newer example

--- is_valid_tri/__init__.py
+++ is_valid_tri/__init__.py
@@ -1,28 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 29 18:28:44 2021
-
-@author: marianne.bezaire
-"""
+import check50
 
 import check50
+import check50.c
+
 
 @check50.check()
 def exists():
     """is_valid_tri.c exists"""
-    check50.require("is_valid_tri.c")
+    check50.exists("is_valid_tri.c")
 
-@check50.check("exists")
+
+@check50.check(exists)
 def compiles():
     """is_valid_tri.c compiles"""
-    check50.spawn("clang -o is_valid_tri is_valid_tri.c -lcs50 -lm").exit(0)
+    check50.c.compile("is_valid_tri.c", lcs50=True)
 
-@check50.check("compiles")
+
+@check50.check(compiles)
 def test_valid_tri():
     """Valid triangle yields true"""
-    check50.spawn("./is_valid_tri").stdin("4","2","1").stdout("true\n", "true\n").exit(0)
+    check50.run("./is_valid_tri").stdin("4","2","1").stdout("true\n", "true\n").exit(0)
 
-@check50.check("compiles")
+@check50.check(compiles)
 def test_invalid_tri():
     """Invalid triangle yields false"""
-    check50.spawn("./is_valid_tri").stdin("4","2","7").stdout("false\n", "false\n").exit(0)
+    check50.run("./is_valid_tri").stdin("4","2","7").stdout("false\n", "false\n").exit(0)

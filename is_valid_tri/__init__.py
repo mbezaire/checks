7e0afd059a0501e4f8e92237fb3f5dcf0670981c
Marianne Bezaire <>
2021-09-29 20:36:33 -0400

    removed class

--- is_valid_tri/__init__.py
+++ is_valid_tri/__init__.py
@@ -5,26 +5,24 @@ Created on Wed Sep 29 18:28:44 2021
 @author: marianne.bezaire
 """
 
-from check50 import *
+import check50
 
-class ValidTri(Checks):
+@check()
+def exists():
+        """is_valid_tri.c exists"""
+        check50.require("is_valid_tri.c")
 
-	@check()
-	def exists(self):
-		"""is_valid_tri.c exists"""
-		self.require("is_valid_tri.c")
+@check("exists")
+def compiles():
+        """is_valid_tri.c compiles"""
+        check50.spawn("clang -o is_valid_tri is_valid_tri.c -lcs50 -lm").exit(0)
 
-	@check("exists")
-	def compiles(self):
-		"""is_valid_tri.c compiles"""
-		self.spawn("clang -o is_valid_tri is_valid_tri.c -lcs50 -lm").exit(0)
+@check("compiles")
+def test_valid_tri():
+        """Valid triangle yields true"""
+        check50.spawn("./is_valid_tri").stdin("4","2","1").stdout("true\n", "true\n").exit(0)
 
-	@check("compiles")
-	def test_valid_tri(self):
-		"""Valid triangle yields true"""
-		self.spawn("./is_valid_tri").stdin("4","2","1").stdout("true\n", "true\n").exit(0)
-
-	@check("compiles")
-	def test_invalid_tri(self):
-		"""Invalid triangle yields false"""
-		self.spawn("./is_valid_tri").stdin("4","2","7").stdout("false\n", "false\n").exit(0)
+@check("compiles")
+def test_invalid_tri():
+        """Invalid triangle yields false"""
+        check50.spawn("./is_valid_tri").stdin("4","2","7").stdout("false\n", "false\n").exit(0)

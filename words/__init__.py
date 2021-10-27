e74b31e4ab4b9e861f70f6f9d4f0d0fb7b99cb60
Marianne Bezaire <marianne.bezaire@andoverma.us>
2021-10-27 13:18:01 -0400

    Update __init__.py

--- words/__init__.py
+++ words/__init__.py
@@ -22,5 +22,5 @@ def compiles():
 
 @check50.check(compiles)
 def words_pos():
-    """When words are used with spaces around them, they are counted correctly"""
+    """When the unwanted words are used with spaces around them, they are counted correctly"""
     check50.run("./words").stdout("very: 2\nstuff: 0\ninterestingly: 0\nseems: 0\nthings: 0\nlike: 2").exit(0)

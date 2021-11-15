75e83d5fc6adce51d753bd3638fed7f5a2c4fc56
Marianne Bezaire <marianne.bezaire@andoverma.us>
2021-11-15 15:17:27 -0500

    Update __init__.py

--- encode/__init__.py
+++ encode/__init__.py
@@ -19,11 +19,11 @@ def compiles():
     check50.c.compile("encode.c", lcs50=True)
 
 @check50.check(compiles)
-def short_phrase():
-    """A phrase is encoded"""
-    check50.run("./encode").stdin("today is monday\n").stdout("tid osa dmy ao yn", "tid osa dmy ao yn\n").exit(0)
-
-@check50.check(compiles)
 def spacey_word():
     """A phrase with several spaces is encoded"""
     check50.run("./encode").stdin("csAP  is    a super   awesome  class").stdout("caae sswc Auel Ppsa ieos srms", "caae sswc Auel Ppsa ieos srms\n").exit(0)
+
+@check50.check(compiles)
+def short_phrase():
+    """A phrase is encoded"""
+    check50.run("./encode").stdin("today is monday").stdout("tid osa dmy ao yn", "tid osa dmy ao yn\n").exit(0)

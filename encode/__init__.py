22ea3fff7a085915a69bdf6b463cf08be2fd94c7
Marianne Bezaire <marianne.bezaire@andoverma.us>
2021-11-15 13:43:42 -0500

    add encode

---
+++ encode/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 18:28:44 2021
+
+@author: marianne.bezaire
+"""
+
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """encode.c exists"""
+    check50.exists("encode.c")
+
+@check50.check(exists)
+def compiles():
+    """encode.c compiles"""
+    check50.c.compile("encode.c", lcs50=True)
+
+@check50.check(compiles)
+def short_phrase():
+    """A phrase is encoded"""
+    check50.run("./encode").stdin("today is monday").stdout("tid osa dmy ao yn", "tid osa dmy ao yn\n").exit(0)
+
+@check50.check(compiles)
+def three_word():
+    """A phrase with numbers is encoded"""
+    check50.run("./encode").stdin("cs50 is a super awesome class").stdout("caae sswc 5uel 0psa ieos srms", "caae sswc 5uel 0psa ieos srms\n").exit(0)

d9d91aef5cfe1ef5d68172defba8c962842d874a
Marianne Bezaire <marianne.bezaire@andoverma.us>
2021-11-22 11:56:30 -0500

    add enlarge

---
+++ enlarge/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 18:28:44 2021
+
+@author: marianne.bezaire
+"""
+
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """enlarge.c exists"""
+    check50.exists("enlarge.c")
+
+@check50.check(exists)
+def compiles():
+    """enlarge.c compiles - Further manual checks will happen!"""
+    check50.c.compile("enlarge.c", lcs50=True)

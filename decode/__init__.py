edcd4dbe13030c99136a6ca135a16d2ae2d4edf6
Marianne Bezaire <marianne.bezaire@andoverma.us>
2021-11-30 12:48:49 -0500

    trblsht 4

--- decode/__init__.py
+++ decode/__init__.py
@@ -19,6 +19,13 @@ def compiles():
     check50.c.compile("decode.c", lcs50=True)
 
 @check50.check(compiles)
-def spacey_word():
+def spacey_phrase():
     """A phrase is decoded"""
     check50.run("./decode").stdin("caae sswc Auel Ppsa ieos srms").stdout("csAPisasuperawesomeclass", "csAPisasuperawesomeclass\n").exit(0)
+
+
+@check50.check(compiles)
+def long_phrase():
+    """A long phrase is decoded"""
+    check50.run("./decode").stdin("iggnatf fehwnho ytaiier otstnny uhehjgo craouou aosuro nuotyd").stdout("ifyoucangetthroughaseasonwithoutaninjurythengoodforyou", "ifyoucangetthroughaseasonwithoutaninjurythengoodforyou\n").exit(0)
+

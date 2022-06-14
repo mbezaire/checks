836a0b3983a87b8116962db18e9ca2aa0ae53697
Marianne Bezaire <marianne.bezaire@andoverma.us>
2022-06-14 13:30:06 -0400

    Update F1Client.java

--- java/fraction/F1Client.java
+++ java/fraction/F1Client.java
@@ -6,9 +6,9 @@ public class F1Client
    {
       Scanner reader = new Scanner(System.in);
       
-      System.out.println("Enter the numerator:")
+      System.out.println("Enter the numerator:");
       int n = reader.nextInt();
-      System.out.println("Enter the denominator:")
+      System.out.println("Enter the denominator:");
       int d = reader.nextInt();
       Fraction quarter = new Fraction(n, d);
       System.out.println(quarter);

a41337ce8f779943ad4b4ca4b63f7ec66b754dc1
Marianne Bezaire <marianne.bezaire@andoverma.us>
2022-06-14 18:26:50 -0400

    Update __init__.py

--- java/fraction-challenge/__init__.py
+++ java/fraction-challenge/__init__.py
@@ -15,25 +15,25 @@ check50.log(out)
 out = check50.run("ls ..").stdout()
 check50.log(out)
 
-less = check50.import_checks("../fraction")
-from less import *
+# less = check50.import_checks("../fraction")
+# from less import *
 
 
-@check50.check(compiles)
-def reduce():
-    """Fractions can be reduced"""
-    out = check50.run("javac -d ./ F6Client.java").stdout(timeout = 60)
-    # check50.log(out)
-    # check50.log(check50.run("pwd").stdout())
-    # check50.log(check50.run("ls ./").stdout())
-    check50.run("java F6Client").stdin("13", prompt=False).stdin("52", prompt=False).stdout("1/4\n").exit(0)
+# @check50.check(compiles)
+# def reduce():
+#     """Fractions can be reduced"""
+#     out = check50.run("javac -d ./ F6Client.java").stdout(timeout = 60)
+#     # check50.log(out)
+#     # check50.log(check50.run("pwd").stdout())
+#     # check50.log(check50.run("ls ./").stdout())
+#     check50.run("java F6Client").stdin("13", prompt=False).stdin("52", prompt=False).stdout("1/4\n").exit(0)
 
 
-@check50.check(compiles)
-def makedouble():
-    """Fractions can be constructed from a double"""
-    out = check50.run("javac -d ./ F7Client.java").stdout(timeout = 60)
-    # check50.log(out)
-    # check50.log(check50.run("pwd").stdout())
-    # check50.log(check50.run("ls ./").stdout())
-    check50.run("java F7Client").stdin("1.25", prompt=False).stdout("5/4\n").exit(0)
+# @check50.check(compiles)
+# def makedouble():
+#     """Fractions can be constructed from a double"""
+#     out = check50.run("javac -d ./ F7Client.java").stdout(timeout = 60)
+#     # check50.log(out)
+#     # check50.log(check50.run("pwd").stdout())
+#     # check50.log(check50.run("ls ./").stdout())
+#     check50.run("java F7Client").stdin("1.25", prompt=False).stdout("5/4\n").exit(0)

1875e595ad8207f3641a4f67d34bba480e4e3c8b
Marianne Bezaire <marianne.bezaire@andoverma.us>
2022-06-14 14:15:04 -0400

    test all 4 math methods

--- java/fraction/__init__.py
+++ java/fraction/__init__.py
@@ -55,24 +55,33 @@ def divide():
     check50.log(out)
     check50.log(check50.run("pwd").stdout())
     check50.log(check50.run("ls ./").stdout())
-    check50.run("/opt/jdk-18.0.1.1/bin/java F2Client").stdin("1", prompt=False).stdin("3", prompt=False).stdin("1", prompt=False).stdin("4", prompt=False).stdout("12/1\n").exit(0)
+    check50.run("/opt/jdk-18.0.1.1/bin/java F2Client").stdin("1", prompt=False).stdin("3", prompt=False).stdin("1", prompt=False).stdin("4", prompt=False).stdout("4/3\n").exit(0)
 
 
-# @check50.check(compiles)
-# def multiply():
-#     """Fractions can be multiplied"""
-#     check50.run("javac F3Client.java")
-#     check50._api.run("java F3Client").stdin("1 4 1 3").stdout("1/12\n").exit(0)
+@check50.check(compiles)
+def multiply():
+    """Fractions can be multiplied"""
+    out = check50.run("/opt/jdk-18.0.1.1/bin/javac -d ./ F3Client.java").stdout(timeout = 60)
+    check50.log(out)
+    check50.log(check50.run("pwd").stdout())
+    check50.log(check50.run("ls ./").stdout())
+    check50.run("/opt/jdk-18.0.1.1/bin/java F3Client").stdin("1", prompt=False).stdin("3", prompt=False).stdin("1", prompt=False).stdin("4", prompt=False).stdout("1/12\n").exit(0)
 
 
-# @check50.check(compiles)
-# def add():
-#     """Fractions can be added"""
-#     check50.run("javac F4Client.java")
-#     check50._api.run("java F4Client").stdin("1 4 1 3").stdout("7/12\n").exit(0)
+@check50.check(compiles)
+def add():
+    """Fractions can be added"""
+    out = check50.run("/opt/jdk-18.0.1.1/bin/javac -d ./ F4Client.java").stdout(timeout = 60)
+    check50.log(out)
+    check50.log(check50.run("pwd").stdout())
+    check50.log(check50.run("ls ./").stdout())
+    check50.run("/opt/jdk-18.0.1.1/bin/java F4Client").stdin("1", prompt=False).stdin("3", prompt=False).stdin("1", prompt=False).stdin("4", prompt=False).stdout("7/12\n").exit(0)
 
-# @check50.check(compiles)
-# def subtract():
-#     """Fractions can be subtracted"""
-#     check50.run("javac F5Client.java")
-#     check50._api.run("java F5Client").stdin("1 3 1 4").stdout("1/12\n").exit(0)
+@check50.check(compiles)
+def subtract():
+    """Fractions can be subtracted"""
+    out = check50.run("/opt/jdk-18.0.1.1/bin/javac -d ./ F5Client.java").stdout(timeout = 60)
+    check50.log(out)
+    check50.log(check50.run("pwd").stdout())
+    check50.log(check50.run("ls ./").stdout())
+    check50.run("/opt/jdk-18.0.1.1/bin/java F5Client").stdin("1", prompt=False).stdin("3", prompt=False).stdin("1", prompt=False).stdin("4", prompt=False).stdout("1/12\n").exit(0)

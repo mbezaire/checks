570f8480adcc82b4a5506d4babfe1ef577aa76c6
Marianne Bezaire <marianne.bezaire@andoverma.us>
2022-06-14 18:31:01 -0400

    Update __init__.py

--- java/fraction-challenge/__init__.py
+++ java/fraction-challenge/__init__.py
@@ -9,11 +9,12 @@ import check50
 import check50.c
 
 check50.include("FClient.java", "F1Client.java", "F2Client.java", "F3Client.java", "F4Client.java", "F5Client.java", "F6Client.java", "F7Client.java")
-
-out = check50.run("ls .").stdout()
-check50.log(out)
-out = check50.run("ls ..").stdout()
-check50.log(out)
+def reduce():
+    """Fractions can be reduced"""
+    out = check50.run("ls .").stdout()
+    check50.log(out)
+    out = check50.run("ls ..").stdout()
+    check50.log(out)
 
 # less = check50.import_checks("../fraction")
 # from less import *

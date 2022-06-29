a1e63c78f199cc998c32102c2de8699a67b11dbe
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-06-29 09:40:58 -0400

    wait for prompt

--- rise/seaslug/__init__.py
+++ rise/seaslug/__init__.py
@@ -15,7 +15,7 @@ def exists():
 @check50.check(exists)
 def calc_ex1():
     """No habituation"""
-    check50.run("python seaslug.py").stdin("0", prompt=False).stdout("[0, 0, 4, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0]", timeout = 60).exit(0)
+    check50.run("python seaslug.py").stdin("0", prompt=True).stdout("[0, 0, 4, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0, 0.0]", timeout = 60).exit(0)
 
 
 @check50.check(exists)

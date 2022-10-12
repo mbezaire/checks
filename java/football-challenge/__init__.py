7f150026b25a687e4b6d68ca1b977c7c620b6296
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-10-12 12:28:51 -0400

    football challenge

---
+++ java/football-challenge/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 18:28:44 2021
+
+@author: marianne.bezaire
+"""
+
+import re
+import check50
+import check50.c
+
+
+@check50.check()
+def exists():
+    """Football.java exists"""
+    check50.exists("Football.java")
+
+@check50.check(exists)
+def compiles():
+    """Football.java compiles"""
+    out = check50.run("javac -d ./ Football.java 2>&1").stdout(timeout = 60)
+    if "error" in out:
+        finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
+        if finderror != None:
+            result = finderror.groups()
+            raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
+        else:
+            raise check50.Failure("Failed to compile", help=finderror)
+            
+
+@check50.check(compiles)
+def runs():
+    """Football.java says "Go for it" when down by 6 with 5 minutes left in the game"""
+    check50.run("java Grade").stdin("21").stdin("27").stdin("305").stdin("35").stdin("50").stdin("55").stdout("Go for it", timeout = 60)
+
+def kicks():
+    """Football.java says "Go for it" when down by 6 with 5 minutes left in the game"""
+    check50.run("java Grade").stdin("21").stdin("27").stdin("305").stdin("35").stdin("60").stdin("45").stdout("Kick", timeout = 60)

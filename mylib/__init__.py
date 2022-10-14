37c80922328ff32b65560c6f5ad21545cd1b129e
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-10-13 22:19:37 -0400

    diff

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -17,47 +17,39 @@ def exists():
     check50.exists("mylib.h")
 
 @check50.check(exists)
-def compiles():
-    """testmylib.c compiles"""
-    check50.c.compile("testmylib.c", lcs50=True)
-
-@check50.check(exists)
 def mylib():
     """mylib contains isvowel and isconsonant"""
     check50.include("testmylib.c","testmylibvowel.c", "testmylibcons.c")
-    out = check50.run("clang -c mylib.c 2>&1").stdout()
-    check50.log(out)
-    out = check50.run("clang testmylib.c -lcs50 mylib.o -o testmylib 2>&1").stdout()
-    check50.log(out)
-    out = check50.run("ls").stdout()
-    check50.log(out)
-    check50.run("./testmylib").stdin("u").stdout("okay", "okay\n", timeout = 60).exit(0)
+    check50.run("clang -c mylib.c 2>&1")
+    check50.run("clang testmylib.c -lcs50 mylib.o -o testmylib 2>&1").stdout()
+    check50.run("./testmylib").stdin("u").stdout("okay", "okay\n").exit(0)
 
 @check50.check(mylib)
 def isvowelyes():
     """isvowel returns 1 for vowel chars"""
+    check50.include("testmylib.c","testmylibvowel.c", "testmylibcons.c")
     check50.run("clang testmylibvowel.c -lcs50 mylib.o -o testmylibvowel")
-    check50.run("./testmylibvowel").stdin("u").stdout("1", "1\n", timeout = 60).exit(0)
+    check50.run("./testmylibvowel").stdin("u").stdout("1", "1\n").exit(0)
 
-@check50.check(isvowelyes)
+@check50.check(mylib)
 def isvowelno():
     """isvowel returns 0 for nonvowel chars"""
-    check50.run("./testmylibvowel").stdin("z").stdout("0", "0\n", timeout = 60).exit(0)
+    check50.run("./testmylibvowel").stdin("z").stdout("0", "0\n").exit(0)
 
 @check50.check(mylib)
 def isconsonantyes():
     """isconsonant returns 1 for consonant chars"""
     check50.run("clang testmylibcons.c -lcs50 mylib.o -o testmylibcons")
-    check50.run("./testmylibcons").stdin("z").stdout("1", "1\n", timeout = 60).exit(0)
+    check50.run("./testmylibcons").stdin("z").stdout("1", "1\n").exit(0)
 
-@check50.check(isconsonantyes)
+@check50.check(mylib)
 def isconsonantno():
     """isconsonant returns 0 for nonconsonant chars"""
     check50.run("clang testmylibcons.c -lcs50 mylib.o -o testmylibcons")
-    check50.run("./testmylibcons").stdin("a").stdout("0", "0\n", timeout = 60).exit(0)
+    check50.run("./testmylibcons").stdin("a").stdout("0", "0\n").exit(0)
 
-@check50.check(isconsonantyes)
+@check50.check(mylib)
 def isconsonantnope():
     """isconsonant returns 0 for numeric chars"""
     check50.run("clang testmylibcons.c -lcs50 mylib.o -o testmylibcons")
-    check50.run("./testmylibcons").stdin("5").stdout("0", "0\n", timeout = 60).exit(0)
+    check50.run("./testmylibcons").stdin("5").stdout("0", "0\n").exit(0)

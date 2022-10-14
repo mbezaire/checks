b9efbe423443e8fac3ad9b9a9c9deb93d8fecc99
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-10-13 21:07:48 -0400

    print

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -28,7 +28,8 @@ def mylib():
     check50.run("clang testmylib.c -lcs50 mylib.o -o testmylib")
     check50.run("clang testmylibvowel.c -lcs50 mylib.o -o testmylib")
     check50.run("clang testmylibcons.c -lcs50 mylib.o -o testmylib")
-    check50.run("./testmylib").stdin("u").stdout("okay", "okay\n").exit(0)
+    out = check50.run("./testmylib 2>&1").stdin("u").stdout() #"okay", "okay\n").exit(0)
+    print(out)
 
 @check50.check(mylib)
 def isvowelyes():

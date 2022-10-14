35b65033fc8667d502b54256a18f45f4824a4223
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-10-13 21:26:16 -0400

    compile better

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -26,32 +26,32 @@ def mylib():
     check50.include("testmylib.c","testmylibvowel.c", "testmylibcons.c")
     check50.run("clang -c mylib.c")
     check50.run("clang testmylib.c -lcs50 mylib.o -o testmylib")
-    check50.run("clang testmylibvowel.c -lcs50 mylib.o -o testmylib")
-    check50.run("clang testmylibcons.c -lcs50 mylib.o -o testmylib")
-    out = check50.run("./testmylib 2>&1").stdin("u").stdout() #"okay", "okay\n").exit(0)
+    check50.run("clang testmylibvowel.c -lcs50 mylib.o -o testmylibvowel")
+    check50.run("clang testmylibcons.c -lcs50 mylib.o -o testmylibcons")
+    out = check50.run("./testmylib 2>&1").stdin("u").stdout("okay", "okay\n", timeout = 60).exit(0)
     check50.log(out)
 
 @check50.check(mylib)
 def isvowelyes():
     """isvowel returns 1 for vowel chars"""
-    check50.run("./testmylibvowel").stdin("u").stdout("1", "1\n").exit(0)
+    check50.run("./testmylibvowel").stdin("u").stdout("1", "1\n", timeout = 60).exit(0)
 
 @check50.check(mylib)
 def isvowelno():
     """isvowel returns 0 for nonvowel chars"""
-    check50.run("./testmylibvowel").stdin("z").stdout("0", "0\n").exit(0)
+    check50.run("./testmylibvowel").stdin("z").stdout("0", "0\n", timeout = 60).exit(0)
 
 @check50.check(mylib)
 def isconsonantyes():
     """isconsonant returns 1 for consonant chars"""
-    check50.run("./testmylibcons").stdin("z").stdout("1", "1\n").exit(0)
+    check50.run("./testmylibcons").stdin("z").stdout("1", "1\n", timeout = 60).exit(0)
 
 @check50.check(mylib)
 def isconsonantno():
     """isconsonant returns 0 for nonconsonant chars"""
-    check50.run("./testmylibcons").stdin("a").stdout("0", "0\n").exit(0)
+    check50.run("./testmylibcons").stdin("a").stdout("0", "0\n", timeout = 60).exit(0)
 
 @check50.check(mylib)
 def isconsonantnope():
     """isconsonant returns 0 for numeric chars"""
-    check50.run("./testmylibcons").stdin("5").stdout("0", "0\n").exit(0)
+    check50.run("./testmylibcons").stdin("5").stdout("0", "0\n", timeout = 60).exit(0)

ef6116de77c720f254130140126fcdd5f3962b00
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-10-17 21:37:31 -0400

    day of week

---
+++ java/dayofweek/__init__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 18:28:44 2021
+
+@author: marianne.bezaire
+"""
+
+import re
+import check50
+import check50.c
+
+classname = 'DayOfWeek'
+
+@check50.check()
+def exists():
+    f"""{classname}.java exists"""
+    check50.exists(f"{classname}.java")
+
+@check50.check(exists)
+def compiles():
+    f"""{classname}.java compiles"""
+    out = check50.run(f"javac -d ./ {classname}.java 2>&1").stdout(timeout = 60)
+    if "error" in out:
+        finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
+        if finderror != None:
+            result = finderror.groups()
+            raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
+        else:
+            raise check50.Failure("Failed to compile", help=finderror)
+            
+
+@check50.check(compiles)
+def runs():
+    f"""{classname}.java should say Jan 5, 2022 is Wednesday (3)"""
+    check50.run(f"java {classname}").stdin("6").stdin("5").stdout("3", timeout = 60)
+
+@check50.check(compiles)
+def runs60():
+    f"""{classname}.java should say Jan 30, 2023 is Monday (1)"""
+    check50.run(f"java {classname}").stdin("0").stdin("30").stdout("1", timeout = 60)
+
+@check50.check(compiles)
+def runs59():
+    f"""{classname}.java should say Jan 22, 2023 is Sunday (0)"""
+    check50.run(f"java {classname}").stdin("0").stdin("22").stdout("0", timeout = 60)
+ 
+@check50.check(compiles)
+def runs89():
+    f"""{classname}.java should say Jan 13, 2024 is Saturday (6)"""
+    check50.run(f"java {classname}").stdin("1").stdin("13").stdout("6S", timeout = 60)
+

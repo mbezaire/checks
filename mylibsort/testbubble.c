e2f5fb92867dfe76a4db1d1bd1ffbf7a70c483fd
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-10-25 09:17:07 -0400

    update

--- mylibsort/testbubble.c
+++ mylibsort/testbubble.c
@@ -6,7 +6,7 @@ int main(void)
 {
 	int myarray[] = {9,4,7,3,1};
 	int otherarray[] = {3,1,8,4,7,3,1};
-	bubblesort(otherarray);
+	bubblesort(otherarray, 7);
     for (int i = 0; i < 7; i++)
       printf("%i,", otherarray[i]);
 }
--- mylibsort/testselection.c
+++ mylibsort/testselection.c
@@ -6,7 +6,7 @@ int main(void)
 {
 	int myarray[] = {9,4,7,3,1};
 	int otherarray[] = {3,1,8,4,7,3,1};
-	selectionsort(myarray); //    bubblesort(otherarray);
+	selectionsort(myarray, 5); //    bubblesort(otherarray);
    for (int i = 0; i < 5; i++)
       printf("%i,", myarray[i]);
 }

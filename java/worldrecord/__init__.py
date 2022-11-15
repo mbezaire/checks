b730a13ffdf0c132f7c6ab93e970c4a4afceb2d0
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-11-15 13:44:38 -0500

    world record

---
+++ java/worldrecord/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 18:28:44 2021
+
+@author: marianne.bezaire
+"""
+
+import re
+import check50
+import check50.c
+
+classname = 'WorldRecord'
+
+@check50.check()
+def exists():
+    """Program file exists"""
+    check50.exists(f"{classname}.java")
+
+@check50.check(exists)
+def compiles():
+    """Program compiles - no logic checks yet"""
+    out = check50.run(f"javac -d ./ {classname}.java 2>&1").stdout(timeout = 60)
+    if "error" in out:
+        finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
+        if finderror != None:
+            result = finderror.groups()
+            raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
+        else:
+            raise check50.Failure("Failed to compile", help=finderror)
+            
+
+#@check50.check(compiles)
+#def runs():
+#    """Program should say Jan 5, 2022 is Wednesday (3)"""
+#    check50.run(f"java {classname}").stdin("6").stdin("5").stdout("3", timeout = 60)
+

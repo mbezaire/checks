c8563551d74e2bc29683738d5867e92350ab46a0
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-11-19 17:48:56 -0500

    reorder encode

--- encode/__init__.py
+++ encode/__init__.py
@@ -19,19 +19,11 @@ def compiles():
     check50.c.compile("encode.c", lcs50=True)
 
 @check50.check(compiles)
-def spacey_word():
-    """A phrase with several spaces is encoded"""
-    out = check50.run("./encode").stdin("csAP  is    a super   awesome  class").stdout()
-    if out.strip() != "caae sswc Auel Ppsa ieos srms":
-        raise check50.Mismatch("caae sswc Auel Ppsa ieos srms\n", out, help='Can your code skip multiple spaces in a row?')
-
-@check50.check(compiles)
 def short_phrase():
     """A phrase is encoded"""
     out = check50.run("./encode").stdin("today is monday").stdout()
     if out.strip() != "tid osa dmy ao yn":
         raise check50.Mismatch("tid osa dmy ao yn\n", out, help='Mondays ARE tough')
-
 
 @check50.check(compiles)
 def other_phrase():
@@ -39,3 +31,10 @@ def other_phrase():
     out = check50.run("./encode").stdin("today is friday").stdout()
     if out.strip() != "tid osa dfy ar yi":
         raise check50.Mismatch("tid osa dfy ar yi\n", out, help='Hmm, better try again')
+
+@check50.check(compiles)
+def spacey_word():
+    """A phrase with several spaces is encoded"""
+    out = check50.run("./encode").stdin("csAP  is    a super   awesome  class").stdout()
+    if out.strip() != "caae sswc Auel Ppsa ieos srms":
+        raise check50.Mismatch("caae sswc Auel Ppsa ieos srms\n", out, help='Can your code skip multiple spaces in a row?')

ae4f02c2d217c01eb8f126186cf37902af5355a3
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2022-11-29 09:50:04 -0500

    time program updates

--- day35/__init__.py
+++ day35/__init__.py
@@ -10,12 +10,27 @@ import check50.c
 
 @check50.check()
 def exists():
-    """struct_address.c and time_program.c were submitted"""
-    check50.exists("struct_address.c")
+    """time_program.c was submitted"""
     check50.exists("time_program.c")
 
 @check50.check(exists)
 def compiles():
-    """struct_address.c and time_program.c compile"""
-    check50.c.compile("struct_address.c", lcs50=True)
+    """time_program.c compiles"""
     check50.c.compile("time_program.c", lcs50=True)
+
+
+@check50.check(compiles)
+def runs():
+    """time_program.c runs"""
+    check50.c.run("./time_program").stdin("02:31:15").stdin("12:05:00").stdout("09:33:45").exit(0)
+
+
+@check50.check()
+def dexists():
+    """struct_address.c was submitted"""
+    check50.exists("struct_address.c")
+
+@check50.check(dexists)
+def dcompiles():
+    """struct_address.c compiles"""
+    check50.c.compile("struct_address.c", lcs50=True)

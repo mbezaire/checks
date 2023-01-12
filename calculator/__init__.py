1c6c2164a98a61ee633f6537d9871ceeba4e1ba7
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-01-12 11:39:38 -0500

    floating point error

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -21,18 +21,22 @@ def compiles():
 @check50.check(compiles)
 def calc_ex1():
     """+ - 3 4 5 yields"""
-    check50.run("./calculator + - 3 4 5").stdout("4.000000\n", "4.000000\n").exit(0)
-
+    out = check50.run("./calculator + - 3 4 5").stdout()
+    if abs(float(out) - 4.000000) > 0.00001:
+        raise check50.Mismatch(out, 4.000000, help='Answer should be within 0.00001 of the expected value')
 
 @check50.check(compiles)
 def calc_ex2():
     """x - 3.4 5.6 7.9 yields"""
-    check50.run("./calculator x - 3.4 5.6 7.9").stdout("-17.379999\n", "-17.379999\n").exit(0)
+    out = check50.run("./calculator x - 3.4 5.6 7.9").stdout()
+    if abs(float(out) - -17.379999) > 0.00001:
+        raise check50.Mismatch(out, -17.379999, help='Answer should be within 0.00001 of the expected value')
 
 
 @check50.check(compiles)
 def calc_ex3():
     """+ - / 4 2 24 x 8 9 yields"""
-    check50.run("./calculator + - / 4 2 24 x 8 9").stdout("50.000000\n", "50.000000\n").exit(0)
-
+    out = check50.run("./calculator + - / 4 2 24 x 8 9").stdout()
+    if abs(float(out) - 50.000000) > 0.00001:
+        raise check50.Mismatch(out, 50.000000, help='Answer should be within 0.00001 of the expected value')
 

9ed25b8e89e0c3cbaf6782bbf25b63e4138edf6f
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-01-13 19:48:13 -0500

    fix calc

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -23,20 +23,19 @@ def calc_ex1():
     """+ - 3 4 5 yields"""
     out = check50.run("./calculator + - 3 4 5").stdout()
     if abs(float(out) - 4.000000) > 0.00001:
-        raise check50.Mismatch(out, 4.000000, help='Answer should be within 0.00001 of the expected value')
+        raise check50.Mismatch(str(4.000000), out, help='Answer should be within 0.00001 of the expected value')
 
 @check50.check(compiles)
 def calc_ex2():
     """x - 3.4 5.6 7.9 yields"""
     out = check50.run("./calculator x - 3.4 5.6 7.9").stdout()
     if abs(float(out) - -17.379999) > 0.00001:
-        raise check50.Mismatch(out, -17.379999, help='Answer should be within 0.00001 of the expected value')
-
+        raise check50.Mismatch(str(-17.379999), out, help='Answer should be within 0.00001 of the expected value')
 
 @check50.check(compiles)
 def calc_ex3():
     """+ - / 4 2 24 x 8 9 yields"""
     out = check50.run("./calculator + - / 4 2 24 x 8 9").stdout()
     if abs(float(out) - 50.000000) > 0.00001:
-        raise check50.Mismatch(out, 50.000000, help='Answer should be within 0.00001 of the expected value')
+        raise check50.Mismatch(str(50.000000), out, help='Answer should be within 0.00001 of the expected value')
 

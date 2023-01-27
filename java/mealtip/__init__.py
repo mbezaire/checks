aeac9c3716f5e4efa9bdfce4c33a21d4d8026525
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-01-27 08:19:19 -0500

    Update __init__.py

--- java/mealtip/__init__.py
+++ java/mealtip/__init__.py
@@ -6,9 +6,8 @@ Created on Wed Sep 29 18:28:44 2021
 """
 
 import check50
-import check50.c
-
-# check50.include("Client.java", "M1Client.java")
+import re
+import math
 
 @check50.check()
 def exists():
@@ -18,13 +17,23 @@ def exists():
 @check50.check(exists)
 def compiles():
     """MealTip.java compiles"""
-    check50.run("javac MealTip.java")
+    out = check50.run("javac -d ./ MealTip.java").stdout(timeout = 60)
+    out = out.replace("Picked up JAVA_TOOL_OPTIONS: -Dsun.java2d.opengl=true","")
+    if "error" in out:
+        finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
+        if finderror != None:
+            result = finderror.groups()
+            raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
+        else:
+            raise check50.Failure("Failed to compile", help=finderror)
+            
+@check50.check(compiles)
+def runs():
+    """MealTip.java runs"""
+    check50.include("Client.java", "M1Client.java")
+    check50.run("javac Client.java M1Client.java")
+    out = check50.run("java MealTip").stdin("25.50").stdin("20").stdout()
+    if math.abs(float(out) - 25.50*1.2) > 0.0001:
+        raise check50.Mismatch(25.50*1.2, out)
 
-# @check50.check(compiles)
-# def runs():
-#     """MealTip.java runs"""
-#     # check50.log(check50.run("pwd").stdout())
-#     # check50.log(check50.run("ls ./").stdout())
-#     check50.run("javac MealTip.java")
-#     out = check50.run("java MealTip").stdout()
-#     check50.log(out)
+

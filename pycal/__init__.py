0c48f22e6575c076d14ffab1c3820a0b53131425
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-02-07 09:24:21 -0500

    pycal 1

---
+++ pycal/__init__.py
@@ -0,0 +1,11 @@
+import check50
+
+@check50.check()
+def exists():
+    """ Check that pycal.py exists """
+    check50.exists("pycal.py")
+
+@check50.check(exists)
+def run():
+    """ File runs without syntax errors and calculates day correctly"""
+    check50.run("python pycal.py").stdin("2").stdout("Wednesday", "Wednesday\n", timeout=5).exit()

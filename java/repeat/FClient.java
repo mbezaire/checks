dcdcab9d9516323cfdf9759c0974173bae2ed3ad
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-02-16 13:18:20 -0500

    tr try ahain

--- java/repeat/FClient.java
+++ java/repeat/FClient.java
@@ -1,10 +1,11 @@
 // Dr B - read and print file
-
+import java.util.Scanner;
 public class FClient
 {
    public static void main(String[] args)
    {
       Repeat repeatTest = new Repeat();
-      System.out.println(repeatTest.main(null));
+      Scanner input = new Scanner(System.in);
+      System.out.println(repeatTest.repeat(input.nextLine()));
    }
 }
--- java/repeat5/FClient.java
+++ java/repeat5/FClient.java
@@ -1,10 +1,11 @@
 // Dr B - read and print file
-
+import java.util.Scanner;
 public class FClient
 {
    public static void main(String[] args)
    {
       Repeat5 repeatTest = new Repeat5();
-      System.out.println(repeatTest.main(null));
+      Scanner input = new Scanner(System.in);
+      System.out.println(repeatTest.repeat5(input.nextLine()));
    }
 }

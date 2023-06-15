71e6408ff10f8f5f5e632902bc18f66a93dfff89
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-06-15 11:29:33 -0400

    ugh g

--- java/tadpole/__init__.py
+++ java/tadpole/__init__.py
@@ -13,8 +13,14 @@ def fix(out):
     return out.replace("Picked up JAVA_TOOL_OPTIONS: -Dsun.java2d.opengl=true\n", "").strip()
 
 def checkit(out, answer):
-    if not match(answer, fix(out)):
-        raise check50.Mismatch(answer, fix(out))
+    flag = True
+    for item in answer:
+        if item not in out:
+            flag = False
+            break
+
+    if not flag:
+        raise check50.Mismatch("...\n".join(answer), fix(out))
 
 
 @check50.check()
@@ -27,17 +33,17 @@ def compiles():
 def runs1():
     """Tadpole.java runs a full frog population correctly"""
     out = check50.run("java Tadpole").stdin("1234").stdout(timeout = 290)
-    checkit(out, "1234 eggs left at day [0-9]+\n135 tadpoles left at day [0-9]+\n36 froglets left at day [0-9]+\n6 frogs left at day [0-9]+")
+    checkit(out, ["1234 eggs left at day ","135 tadpoles left at day ","36 froglets left at day ","6 frogs left at day "])
 
 @check50.check(compiles)
 def runs2():
     """Tadpole.java ensures realistic nest size"""
     out = check50.run("java Tadpole").stdin("2392").stdout(timeout = 290)
-    checkit(out, "2000 eggs left at day [0-9]+\n220 tadpoles left at day [0-9]+\n59 froglets left at day [0-9]+\n10 frogs left at day [0-9]+")
+    checkit(out, ["2000 eggs left at day ","220 tadpoles left at day ","59 froglets left at day ","10 frogs left at day "])
 
 
 @check50.check(compiles)
 def runs3():
     """Tadpole.java ensures realistic nest size on other end"""
     out = check50.run("java Tadpole").stdin("-55").stdout(timeout = 290)
-    checkit(out, "1 eggs left at day [0-9]+\n0 tadpoles left at day [0-9]+\n0 froglets left at day [0-9]+\n0 frogs left at day [0-9]+")
+    checkit(out, ["1 eggs left at day ","0 tadpoles left at day ","0 froglets left at day ","0 frogs left at day "])

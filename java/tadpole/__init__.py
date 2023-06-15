6a903f31be891bc9067c5b87bd3bfa6cf1bb7287
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-06-15 11:54:55 -0400

    maybe

--- java/tadpole/__init__.py
+++ java/tadpole/__init__.py
@@ -31,12 +31,14 @@ def compiles():
 @check50.check(compiles)
 def runs1():
     """Tadpole.java runs a full frog population correctly"""
+    out = check50.run("javac -d ./ Tadpole.java").stdout(timeout = 180)
     out = check50.run("java Tadpole").stdin("1234").stdout(timeout = 1000)
     checkit(out, ["1234 eggs left at day ","135 tadpoles left at day ","36 froglets left at day ","6 frogs left at day "])
 
 @check50.check(compiles)
 def runs2():
     """Tadpole.java ensures realistic nest size"""
+    out = check50.run("javac -d ./ Tadpole.java").stdout(timeout = 180)
     out = check50.run("java Tadpole").stdin("2392").stdout(timeout = 1000)
     checkit(out, ["2000 eggs left at day ","220 tadpoles left at day ","59 froglets left at day ","10 frogs left at day "])
 
@@ -44,5 +46,6 @@ def runs2():
 @check50.check(compiles)
 def runs3():
     """Tadpole.java ensures realistic nest size on other end"""
+    out = check50.run("javac -d ./ Tadpole.java").stdout(timeout = 180)
     out = check50.run("java Tadpole").stdin("-55").stdout(timeout = 1000)
     checkit(out, ["1 eggs left at day ","0 tadpoles left at day ","0 froglets left at day ","0 frogs left at day "])

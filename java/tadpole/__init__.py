dec54131228189573a4e1ce2c9c202f4ce3c1261
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-06-15 11:24:58 -0400

    next

--- java/tadpole/__init__.py
+++ java/tadpole/__init__.py
@@ -26,18 +26,18 @@ def compiles():
 @check50.check(compiles)
 def runs1():
     """Tadpole.java runs a full frog population correctly"""
-    out = check50.run("java Tadpole").stdin("1234").stdout(timeout = 120)
+    out = check50.run("java Tadpole").stdin("1234").stdout(timeout = 290)
     checkit(out, "1234 eggs left at day [0-9]+\n135 tadpoles left at day [0-9]+\n36 froglets left at day [0-9]+\n6 frogs left at day [0-9]+")
 
 @check50.check(compiles)
 def runs2():
     """Tadpole.java ensures realistic nest size"""
-    out = check50.run("java Tadpole").stdin("2392").stdout(timeout = 120)
+    out = check50.run("java Tadpole").stdin("2392").stdout(timeout = 290)
     checkit(out, "2000 eggs left at day [0-9]+\n220 tadpoles left at day [0-9]+\n59 froglets left at day [0-9]+\n10 frogs left at day [0-9]+")
 
 
 @check50.check(compiles)
 def runs3():
     """Tadpole.java ensures realistic nest size on other end"""
-    out = check50.run("java Tadpole").stdin("-55").stdout(timeout = 120)
+    out = check50.run("java Tadpole").stdin("-55").stdout(timeout = 290)
     checkit(out, "1 eggs left at day [0-9]+\n0 tadpoles left at day [0-9]+\n0 froglets left at day [0-9]+\n0 frogs left at day [0-9]+")

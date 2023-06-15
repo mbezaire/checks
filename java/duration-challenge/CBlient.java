417e2b5d30154c21f455aa23d1aea239039d692e
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-06-15 06:53:46 -0400

    hope

--- java/duration-challenge/CBlient.java
+++ java/duration-challenge/CBlient.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class CBlient {
     public static void main(String[] args) {
         Scanner ear = new Scanner(System.in);
+		System.out.println("Enter somethin");
         int num = ear.nextInt();ear.nextLine();
         String units = ear.nextLine();
         Duration test = new Duration(num, units);
+		System.out.println("Enter somethin");
         num = ear.nextInt();ear.nextLine();
         units = ear.nextLine();
         Duration other = new Duration(num, units);
--- java/duration-challenge/CClient.java
+++ java/duration-challenge/CClient.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class CClient {
     public static void main(String[] args) {
         Scanner ear = new Scanner(System.in);
+		System.out.println("Enter somethin");
+
         int num = ear.nextInt();ear.nextLine();
         String units = ear.nextLine();
         Duration test = new Duration(num, units);
+		System.out.println("Enter somethin");
+
         num = ear.nextInt();ear.nextLine();
         units = ear.nextLine();
         Duration other = new Duration(num, units);

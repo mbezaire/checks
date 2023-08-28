83591df9547a1d4894fe5787133c62e3fe5d7b68
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-08-28 18:37:00 -0400

    check that helper files compile in compile step

--- java/bank/__init__.py
+++ java/bank/__init__.py
@@ -70,7 +70,15 @@ def compiles():
             raise check50.Failure("Failed to compile", help=finderror)
     if len(helperfile) > 0:
         out = check50.run(f"javac -d ./ {helperfile}.java").stdout(timeout = 60)
-        check50.log(out)
+        if "error" in out:
+            finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
+            if finderror != None:
+                result = finderror.groups()
+                raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
+            else:
+                raise check50.Failure("Failed to compile", help=finderror)
+        for line in out.split("\n"):
+            check50.log(line)
         out = check50.run(f"java {helperfile}").stdout(timeout = 360)
 
         if "{" in out:

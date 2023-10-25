1f9a1728941bcf51223741b2db962e14ce23b269
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2023-10-25 14:16:37 -0400

    updated essay

--- essay/__init__.py
+++ essay/__init__.py
@@ -22,3 +22,16 @@ def single_sentence_complex():
         check50.log(out2)
         if len(out2) < 20 or "Dvorak" not in out2:
             raise check50.Failure("It seems that your metrics report is lacking...", help="Make sure to print out the readability, all the metrics from readability, any repeated words, and the number of times the word Dvorak and the word what appear in the essay")
+
+
+@check50.check(single_sentence_complex)
+def repeated():
+    """prints out repeated words from essay.txt"""
+    check50.include("essay.txt")
+    out = check50.run("./essaymetrics essay.txt").stdout()
+    check50.log(out)
+    if len(out) < 20 or "most most" not in out or "two two" not in out or "the the" not in out:
+        out2 = check50.run("./essaymetrics essay.txt Dvorak").stdout()
+        check50.log(out2)
+        if len(out2) < 20 or "most most" not in out or "two two" not in out or "the the" not in out:
+            raise check50.Failure("Not all of the repeated words were reported", help="Make sure to print the double word phrase, like 'the the' if there are two thes in a row")

d3af998b3bc432f31c433619e0fef3b76e239f4d
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-01-21 10:10:48 -0500

    python hello check

---
+++ python/hello/__init__.py
@@ -0,0 +1,11 @@
+import check50
+
+@check50.check()
+def exists():
+    """ Check that hello.py exists """
+    check50.exists("hello.py")
+
+@check50.check(exists)
+def run():
+    """ File runs without syntax errors and greets the user correctly"""
+    check50.run("python hello.py").stdin("Mr. Govoni").stdout("[Hh]ello[,]{0,1} Mr. Govoni", "Hello, Mr. Govoni\n", timeout=30, regex=True).exit(0)

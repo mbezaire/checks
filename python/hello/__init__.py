c61bac8cd1fc2c4ba76d13b5badc55ab49d7a6cf
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-01-21 10:25:11 -0500

    add one more check

--- python/hello/__init__.py
+++ python/hello/__init__.py
@@ -9,3 +9,8 @@ def exists():
 def run():
     """ File runs without syntax errors and greets the user correctly"""
     check50.run("python hello.py").stdin("Mr. Govoni").stdout("[Hh]ello[,]{0,1} Mr. Govoni", "Hello, Mr. Govoni\n", timeout=30, regex=True).exit(0)
+
+@check50.check(exists)
+def run2():
+    """ Program greets another correctly"""
+    check50.run("python hello.py").stdin("there").stdout("[Hh]ello[,]{0,1} there", "Hello, there\n", timeout=30, regex=True).exit(0)

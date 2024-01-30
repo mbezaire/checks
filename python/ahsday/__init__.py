424770ba8ceeb4ec5bc6e090b3443a060656e9ad
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-01-30 13:23:42 -0500

    ahsday basic slug

---
+++ python/ahsday/__init__.py
@@ -0,0 +1,16 @@
+import check50
+
+@check50.check()
+def exists():
+    """ Check that days.py exists """
+    check50.exists("days.py")
+    check50.include("data.txt")
+
+@check50.check(exists)
+def run():
+    """ File runs without syntax errors and prints some days"""
+    out = check50.run("python days.py").stdout(timeout=30)
+    check50.log(out)
+    if len(out) < 10:
+        raise check50.Failure("Make sure your program has some client code that creates a Day, and AHSDay, and prints them out")
+

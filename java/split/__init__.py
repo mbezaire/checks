40fb7c01883b96e36238c3d6b00a4f240936a2c2
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-02-01 08:26:03 -0500

    add split check

---
+++ java/split/__init__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 18:28:44 2021
+
+@author: marianne.bezaire
+"""
+
+import re
+import check50
+import check50.c
+
+
+@check50.check()
+def exists():
+    """Split.java exists"""
+    check50.exists("Split.java")
+
+@check50.check(exists)
+def compiles():
+    """Split.java compiles"""
+    check50.include("content.txt");
+    out = check50.run("javac -d ./ Split.java 2>&1").stdout(timeout = 60)
+    if "error" in out:
+        finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
+        if finderror != None:
+            result = finderror.groups()
+            raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
+        else:
+            raise check50.Failure("Failed to compile", help=finderror)
+
+@check50.check(compiles)
+def runs():
+    """Split.java prints out an array of Strings"""
+    check50.run("java Split").stdout('["one", "two", "three", "four"]',timeout = 5);
+
+
+@check50.check(compiles)
+def runs40():
+    """Find.java uses a your custom split method"""
+    with open("Split.java") as f:
+        content = f.read()
+        content = content.replace(" (","(")
+    if "split(" not in content or ".split(" in content:
+        raise check50.Failure("Looks like you used the regular String split method instead of your own")
+    content = content.replace(" ","").replace("\n","").replace("\t","")
+    if "publicstaticString[]split(Stringwhole,chardelimiter){}" in content:
+        raise check50.Failure("Looks like you forgot to write your own split method")
+

46a2e8c657b57af2a2f8c0f402ca9fd0a34c2c24
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-03-06 09:07:29 -0500

    arraylist draft

---
+++ arrraylist/draft/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 18:28:44 2021
+
+@author: marianne.bezaire
+"""
+
+import check50
+import re
+import random
+
+@check50.check()
+def compiles():
+    """Your code compiles"""
+    out = check50.run("javac -d ./ *.java").stdout(timeout = 60)
+    out = out.replace("Picked up JAVA_TOOL_OPTIONS: -Dsun.java2d.opengl=true","")
+    if "error" in out:
+        finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
+        if finderror != None:
+            result = finderror.groups()
+            raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
+        else:
+            raise check50.Failure("Failed to compile", help=finderror)

d9c54f9cfbb54d6631c3486e0170992f4a491844
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-03-08 06:09:27 -0500

    new dog

--- java/dog/__init__.py
+++ java/dog/__init__.py
@@ -30,10 +30,10 @@ def compiles():
 
 @check50.check(compiles)
 def runs():
-    """Dog.java has a dogYears method that works alright"""
+    """Dog.java has a dogYears method that takes human years as an argument and returns corresponding dog years"""
     check50.include("TestDogYears.java","TestPrintDog.java")
     out = check50.run("javac -d ./ TestDogYears.java").stdout(timeout = 60)
-    age = 4
+    age = random.randint(3,9)
     check50.run("java TestDogYears 2>&1").stdin(str(age), prompt = False).stdout(str(age*7),timeout = 30)
 
 @check50.check(runs)
@@ -41,8 +41,9 @@ def printdog():
     """Dog.java has a printDog method that works alright"""
     out = check50.run("javac -d ./ TestPrintDog.java").stdout(timeout = 60)
     check50.log(out)
-    age = 9
+    age = random.randint(1,8)
     out = check50.run("java TestPrintDog 2>&1").stdin("Rocket").stdin("123.2").stdin(str(age)).stdin("Standard Poodle").stdout(timeout = 30)
+    check50.log(out)
 
     if not ("Rocket" in out and "123.2" in out and str(age*7) in out and "Standard Poodle" in out):
-        raise check50.Mismatch(f"Rocket (a Standard Poodle) weighs 123.2 lbs, is {age*7} in dog years, and is a good doggy", out.strip())
+        raise check50.Mismatch(f"Rocket (a Standard Poodle) weighs 123.2 lbs,\nis {age*7} in dog years, and is a good doggy", out.strip(). help="Your print statement can be worded differently, but it should still contain all the information about your dog including the age in dog years")

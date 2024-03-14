ddaca883f798fe5bd8da446c42963117b9acbc97
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-03-14 10:03:18 -0400

    Update __init__.py

--- java/twograde/__init__.py
+++ java/twograde/__init__.py
@@ -32,19 +32,19 @@ def compiles():
 @check50.check(compiles)
 def runs():
     """TwoGrade.java gives an A for two strong grades"""
-    check50.run("java TwoGrade").stdin("91").stdin("95").stdout("Student has an A", timeout = 60)
+    check50.run("java TwoGrade").stdin("91").stdin("95").stdout("[sS]tudent has an A[\.]*", timeout = 60)
 
 @check50.check(compiles)
 def runs60():
     """TwoGrade.java gives a fail for two low grades"""
-    check50.run("java TwoGrade").stdin("50").stdin("55").stdout("[Ss]tudent [Ff]ailed", timeout = 60, regex = True)        
+    check50.run("java TwoGrade").stdin("50").stdin("55").stdout("[Ss]tudent [Ff]ailed[\.]*", timeout = 60, regex = True)        
 
 @check50.check(compiles)
 def runs59():
     """TwoGrade.java gives a fail if at least one grade is below 20"""
-    check50.run("java TwoGrade").stdin("19").stdin("110").stdout("[Ss]tudent [Ff]ailed", timeout = 60, regex = True)        
+    check50.run("java TwoGrade").stdin("19").stdin("110").stdout("[Ss]tudent [Ff]ailed[\.]*", timeout = 60, regex = True)        
  
 @check50.check(compiles)
 def runs89():
     """TwoGrade.java gives a double average for other grades"""
-    check50.run("java TwoGrade").stdin("50").stdin("90").stdout("[Ss]tudent has a 70[\.0]* average", timeout = 60, regex = True)        
+    check50.run("java TwoGrade").stdin("50").stdin("90").stdout("[Ss]tudent has [an ]*70[\.0]* average[\.]*", "Student has a 70 average (or 70.0, double or int is okay), timeout = 60, regex = True)        

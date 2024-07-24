563342c965267c8834bf596ef428b16f49b613dd
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-07-24 11:39:33 -0400

    Update __init__.py

--- python/trivia/__init__.py
+++ python/trivia/__init__.py
@@ -7,6 +7,18 @@ def exists():
 
 @check50.check(exists)
 def run():
-    """ File presents the question and answers correctly"""
-    out = check50.run("python trivia.py").stdout(timeout=40).stdin("4").stdout(timeout=30)
-    check50.log(out)
+    """ The correct answer gets congratulated. """
+    check50.run("python trivia.py").stdin("4", prompt = True).stdout("Congrats, you got it!", timeout=30).exit(0)
+
+@check50.check(exists)
+def run2():
+    """ The incorrect answer gets an apology. """
+    for r in range(1,4):
+        check50.run("python trivia.py").stdin(str(r), prompt = True).stdout("Sorry, wrong answer!", timeout=30).exit(0)
+
+# @check50.check(run2)
+# def run3():
+#     """ No spaces between the number and the dot in the menu of choices. """
+#     for r in range(1,4):
+#         check50.run("python trivia.py").stdin(str(r), prompt = True).stdout("Sorry, wrong answer!", timeout=30).exit(0)
+

c9f6a594db13a74723f1631cce5a0164966a61b9
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-07-24 11:13:35 -0400

    take 2

--- python/trivia/__init__.py
+++ python/trivia/__init__.py
@@ -8,5 +8,5 @@ def exists():
 @check50.check(exists)
 def run():
     """ File presents the question and answers correctly"""
-    out = check50.run("python trivia.py").stdout("In which country is it illegal to own just one guinea pig?", timeout=30, regex=True).stdout().stdin("4").stdout()
+    out = check50.run("python trivia.py").stdout("In which country is it illegal to own just one guinea pig?", timeout=30, regex=True).stdout(timeout=40).stdin("4").stdout(timeout=30)
     check50.log(out)

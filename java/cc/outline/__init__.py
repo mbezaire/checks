5aaade251af5afccfc8f8ac1182e5e54f77f53bb
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-09-20 08:48:28 -0400

    custom class outline

---
+++ java/cc/outline/__init__.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 18:28:44 2021
+
+@author: marianne.bezaire
+"""
+
+import check50
+import re
+import random
+import os
+
+@check50.check()
+def exists():
+    """Your 3 class files, image file, and README exist"""
+    check50.exists("README.md")
+    with open("README.md") as f:
+        text = f.read()
+
+    if len(text) < 10:
+        raise check50.Failure("Add more content to your README.", help="Include notes about class content and inheritance syntax")
+    elif '#' not in text:
+        raise check50.Failure("Include a header in your README.", help="Use a # symbol, then a space, then your heading")
+    elif text.replace(" ","").count('![') < 1:
+        raise check50.Failure("Make sure to include the UML image in your README.")
+
+    files = os.listdir()
+    flag = False
+    java_files = []
+    for file in files:
+        if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
+            flag = True
+        elif file.endswith(".java"):
+            java_files.append(file)
+
+    if len(java_files) < 3:
+        raise check50.Failure("Make sure to include at least 3 Java files. So far you have: " + ", ".join(java_files))
+    elif not flag:
+        raise check50.Failure("Make sure to generate a UML diagram at https://yuml.me and save it as an image (png, jpg, or jpeg) in your custom class folder")
+    return java_files
+
+@check50.check(exists)
+def compiles(java_files):
+    """All java files compile"""
+    out = check50.run("javac -d ./ *.java").stdout(timeout = 60)
+    out = out.replace("Picked up JAVA_TOOL_OPTIONS: -Dsun.java2d.opengl=true","")
+    if "error" in out:
+        finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
+        if finderror != None:
+            result = finderror.groups()
+            raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
+        else:
+            raise check50.Failure("Failed to compile", help=finderror)
+    return java_files
+
+@check50.check(compiles)
+def relations(java_files):
+    """Checking for HAS A and IS A relations"""
+    hasa = False
+    isa = False
+    extends = False
+    helptext = ''
+
+    for file in java_files:
+        with open(file) as f:
+            text = f.read()
+
+        if "extends" in text:
+            extends = True
+            myclass = text[text.find("extends") + 7 : text.find("{", text.find("extends") + 7)].strip()
+            if myclass + ".java" in java_files and (not isa or len(helptext) > 0):
+                isa = True
+            else:
+                helptext += "We found an extends statement in your file " + file + " but the class it extends was " + myclass + " and you didn't upload anything by that name.\n"
+
+        for cl in java_files:
+            if cl[:-5] in text[text.find("{"):]:
+                hasa = True
+    
+    if extends == False:
+        helptext += "We didn't find an extends statement in any of your files. You need at least one.\n"
+    
+    if hasa == False:
+        helptext += "We didn't find a field in any of your classes that represents an object of one of your other classes. You need at least one.\n" 
+
+    if not isa or not hasa:
+        raise check50.Failure("You need both a HAS A and an IS A relationship among your 3 classes", help=helptext)

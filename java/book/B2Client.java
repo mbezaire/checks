7f6069cb152267d64cfb5b3e4fb9dc042a2d8831
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-09-25 10:29:38 -0400

    fix typo

--- java/book/B2Client.java
+++ java/book/B2Client.java
@@ -4,10 +4,10 @@ public class B2Client
 {
    public static void main(String[] args)
    {
-      Book mybook = new Book("An Other World","Edward is Young",5.43);
+      Book myBook = new Book("An Other World","Edward is Young",5.43);
       myBook.setTitle("An Immense World");
       myBook.getAuthor().setName("Ed Yong");
       myBook.setPrice(85.43);
-      System.out.println(mybook);
+      System.out.println(myBook);
    }
 }

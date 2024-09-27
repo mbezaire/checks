b56bb3504f5df3e4e27adf973e3e8442133c5cb9
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-09-27 08:34:11 -0400

    check client output

--- java/bookstore/__init__.py
+++ java/bookstore/__init__.py
@@ -8,65 +8,28 @@ Created on Wed Sep 29 18:28:44 2021
 import check50
 import check50.c
 
-# not working in sandbox
-# less = check50.import_checks("https://github.com/mbezaire/checks/main/java/fraction")
-# from less import *
-
-
 @check50.check()
 def exists():
-    """Book.java exists"""
+    """Book.java, Author.java, and BClient.java exist"""
     check50.exists("Book.java")
-
-
-@check50.check()
-def authexists():
-    """Author.java exists"""
     check50.exists("Author.java")
-
-@check50.check()
-def authexists():
-    """BClient.java exists"""
     check50.exists("BClient.java")
     
 @check50.check(exists)
 def compiles():
-    """Book.java compiles"""
-    check50.run("javac Book.java")
-    
-@check50.check(authexists)
-def authcompiles():
-    """Author.java compiles"""
-    check50.run("javac Author.java")
-    
-@check50.check(authexists)
-def authcompiles():
-    """BClient.java compiles"""
-    check50.run("javac BClient.java")
-
-
-# @check50.check(compiles)
-# def runs():
-#     """Book.java runs"""
-#     out = check50.run("javac -d ./ BClient.java").stdout(timeout = 60)
-#     check50.log(out)
-#     check50.log(check50.run("pwd").stdout())
-#     check50.log(check50.run("ls ./").stdout())
-#     out2 = check50.run("java BClient").stdout()
-#     check50.log(out2)
-
-
-# @check50.check(compiles)
-# def reduce():
-#     """A book has a price and a toString"""
-#     out = check50.run("javac -d ./ B1Client.java").stdout(timeout = 60)
-
-    
-#     from re import match
-
-#     expected = "[Aa]n {Ii]mmense [Ww]orld[\n\t\s]*[bByY :]+Ed Yong[\n\t\s]*[$0-9\.]+"
-#     actual = check50.run("java B1Client").stdout()
-#     if not match(expected, actual):
-#         help = None
-#         raise check50.Mismatch("An Immense World\nBy Ed Yong\n$85.43", actual, help=help)
-
+    """Everything compiles"""
+    out = check50.run(f"javac -d ./ *.java 2>&1").stdout(timeout = 60)
+    if "error" in out:
+        finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
+        if finderror != None:
+            result = finderror.groups()
+            raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
+        else:
+            raise check50.Failure("Failed to compile", help=finderror)
+            
+@check50.check(compiles)
+def runs():
+    """BClient.java runs and prints out info for 3 books that got added to bookstore"""
+    out = check50.run("java BClient.java").stdout(timeout = 60)
+    if len(out) < 50 or out.count("$") < 3 or (out.lower().count("by") + out.lower().count("thor")) < 3:
+        raise check50.Failure("Your BClient needs to print detailed info for 3 separate books.", help="Your BClient only prints:\n" + out)

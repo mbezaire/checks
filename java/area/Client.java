0582de79fb389f69fdad4851e53e52262e1d1a07
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-10-01 14:51:38 -0400

    fix

--- java/area/Client.java
+++ java/area/Client.java
@@ -3,7 +3,7 @@ public class Client {
         double ans = ShapeArea.getCircleArea(1);
         ans += ShapeArea.getSphereArea(1);
         ans += ShapeArea.getRectangleArea(1,1);
-        ans += ShapeArea.getTriangleArea(1);
+        ans += ShapeArea.getTriangleArea(1,1,1);
         System.out.println(ans);
     }
 }

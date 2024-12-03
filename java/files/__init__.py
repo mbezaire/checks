558d2d43e9528f54e6c205c916122bb9a688d409
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2024-12-03 12:25:18 -0500

    no helpers for real

--- java/files/__init__.py
+++ java/files/__init__.py
@@ -11,7 +11,6 @@ import os
 os.environ["CHECK50_WORKERS"] = "1"
 import json
 
-helperfile = ["count.txt","loopcount.txt","info.txt"]
 theirfile = ["ReadAFile", "WriteAFile", "CountAndPrint", "LoopCount"]
 
 ## New plan:
@@ -61,22 +60,6 @@ def compiles():
             raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
         else:
             raise check50.Failure("Failed to compile", help=finderror)
-    if len(helperfile) > 0:
-        out = check50.run(f"javac -d ./ {helperfile}.java").stdout(timeout = 60)
-        if "error" in out:
-            finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
-            if finderror != None:
-                result = finderror.groups()
-                raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
-            else:
-                raise check50.Failure("Failed to compile", help=finderror)
-        for line in out.split("\n"):
-            check50.log(line)
-        out = check50.run(f"java {helperfile}").stdout(timeout = 360)
-
-        if "{" in out:
-            f_results = json.loads(out[out.index("{"):])
-        return f_results
 
 def findCheck(f_results, id = "constructors"):
     for i, test in enumerate(f_results['tests']):

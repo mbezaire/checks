533272a2baeeb48e8c61297a5cab78827140a682
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2025-01-13 10:14:50 -0500

    fix parsonloop

--- java/parsonloop/__init__.py
+++ java/parsonloop/__init__.py
@@ -7,6 +7,7 @@ Created on Wed Sep 29 18:28:44 2021
 
 import check50
 import check50.c
+import re
 
 @check50.check()
 def exists():
@@ -20,6 +21,7 @@ def contents():
 
     with open("ParsonLoop.java") as f:
         contents = f.read().replace('\n','').replace('{','').replace('}','')
+        contents= re.sub(r"\/\*.*?(?=\*\/|\Z)\*/","",contents)
 
     with open("Orig.java") as f:
         lines = f.readlines()
@@ -37,17 +39,17 @@ def contents():
 @check50.check(contents)
 def compiles():
     """Program compiles"""
-    check50.c.compile("parson.c") #, lcs50=True)
-
+    out = check50.run("javac -d ./ ParsonLoop.java 2>&1").stdout(timeout = 60)
+    if "error" in out:
+        finderror = re.search(r'([\s\S]+)?(?=([0-9]+ error[s]{0,1}))', out.replace("Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output",""))
+        if finderror != None:
+            result = finderror.groups()
+            raise check50.Failure("Failed to compile due to " + result[1], help=result[0].strip())
+        else:
+            raise check50.Failure("Failed to compile", help=finderror)
 
 @check50.check(compiles)
 def runs():
-    """Program runs alright"""
-    check50.include("data.txt")
-    check50.run(f"./parson").stdout("Anya: 94\nArnold: 93\nAnwar: 91\nAvery: 95\nAnnalise: 98\nAmy: 90\nAndrew: 92").exit(0)
+    """Program prints out the expected output"""
+    check50.run("java ParsonLoop").stdout("J L N P R T V \nI K M O Q S U \nH J L N P R T \nG I K M O Q S \nF H J L N P R \nE G I K M O QD F H J L N P \nC E G I K M O \nB D F H J L N \nA C E G I K M ").exit(0)
 
-
-@check50.check(runs)
-def valgrinds():
-    """Valgrind likes the program"""
-    check50.c.valgrind(f"./parson").stdout("Anya: 94\nArnold: 93\nAnwar: 91\nAvery: 95\nAnnalise: 98\nAmy: 90\nAndrew: 92").exit(0)

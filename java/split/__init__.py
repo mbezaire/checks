653996c4612c18d23f76faf29dcc6afe1dc57a9f
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2025-01-31 19:42:33 -0500

    Update __init__.py

--- java/split/__init__.py
+++ java/split/__init__.py
@@ -65,9 +65,10 @@ def runs():
         for i in range(min(len(out),len(output))):
             if out[i] != output[i]:
                 check50.log(f'difference at character {i} which is {out[i]} or {output[i]}')
-        print(f'lengths are: {len(out)}, {len(output)}')
+        check50.log(f'lengths are: {len(out)}, {len(output)}')
+        raise check50.Mismatch(output, out)
 
-    check50.run("java TestSplit").stdin(phrase,prompt = False).stdin(delim,prompt = False).stdout(output,timeout = 5)
+    #check50.run("java TestSplit").stdin(phrase,prompt = False).stdin(delim,prompt = False).stdout(output,timeout = 5)
 
 
 

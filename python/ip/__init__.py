c5a094094178c06f4dcf3289f257f7ba051bf9ce
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2025-02-03 18:35:55 -0500

    Update __init__.py

--- python/ip/__init__.py
+++ python/ip/__init__.py
@@ -1,12 +1,12 @@
 import check50
 import random
 
-def getrandinfo():
+def getrandinfo(arg):
     nums = []
     for i in range(8):
         nums.append(str(random.randint(0,255)))
     
-    if int(nums[0]) % 2 == 0:
+    if arg % 2 == 0:
         i = 3
         ans = ".".join(nums[4:])
     else:
@@ -15,9 +15,9 @@ def getrandinfo():
     
     nums[i] = str(int(nums[i]) + 256)
     with open("info.txt","w") as f:
-        f.write(f'How many are in here? 12.124.32 and also {nums[0]}.{nums[1]}.{nums[2]}.{nums[3]} and lastly {nums[4]}.{nums[5]}.{nums[6]}.{nums[7]}')
+        f.write(f'How many are in here? 12.124.32 and also {nums[0]}.{nums[1]}.{nums[2]}.{nums[3]} and lastly {nums[4]}.{nums[5]}.{nums[6]}.{nums[7]} hmm. Oh and 255.0.0.1')
 
-    return ans
+    return ans + "\n255.0.0.1\n"
 
 @check50.check()
 def exists():
@@ -28,14 +28,20 @@ def exists():
 def run():
     """ File runs without syntax errors and finds a valid IP address"""
     ans = getrandinfo()
-    check50.run("python find.py").stdout(ans + "\n", timeout=30).exit(0)
+    out = check50.run("python find.py").stdout(ans, timeout=30)
+    check50.log(out)
+    check50.log("-"*30)
+    check50.log(ans)
 
 @check50.check(run)
 def run2():
     """ Program finds another valid IP address"""
     ans = getrandinfo()
-    check50.run("python find.py").stdout(ans + "\n", timeout=30).exit(0)
-
+    out = check50.run("python find.py").stdout(ans, timeout=30)
+    check50.log(out)
+    check50.log("-"*30)
+    check50.log(ans)
+    
 @check50.check(run2)
 def usere():
     """ Program uses re's findall and for each loops without excessive logic"""

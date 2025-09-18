063eb3fbe9cf36c95f758a599fccd90681956a8a
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2025-09-18 13:06:46 -0400

    Update __init__.py

--- bubble/__init__.py
+++ bubble/__init__.py
@@ -14,22 +14,22 @@ def compiles():
     check50.c.compile("bubblesort.c", lcs50=True)
 
 
-# @check50.check(compiles)
-# def test_valid_tri():
-#     """Bubble sorter sorts alright"""
-#     mynums = ""
-#     nums = []
-#     length = random.randint(3,7)
-#     for _ in range(length):
-#         val = random.randint(10,500)
-#         mynums += " " + str(val)
-#         nums.append(val)
+@check50.check(compiles)
+def test_valid_tri():
+    """Bubble sorter sorts alright"""
+    mynums = ""
+    nums = []
+    length = random.randint(3,7)
+    for _ in range(length):
+        val = random.randint(10,500)
+        mynums += " " + str(val)
+        nums.append(val)
 
-#     nums.sort()
+    nums.sort()
 
-#     corrnums = ""
-#     for num in nums:
-#         corrnums += str(num) + "\n"
+    corrnums = ""
+    for num in nums:
+        corrnums += str(num) + "\n"
 
 
-#     check50.run("./bubblesort" + mynums).stdout(corrnums).exit(0)
+    check50.run("./bubblesort" + mynums).stdout(corrnums).exit(0)

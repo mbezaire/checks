df7de84273148da80913cd15ec186971dbb767de
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2026-02-03 11:14:44 -0500

    restore stocktax

--- python/stocktax/__init__.py
+++ python/stocktax/__init__.py
@@ -5,9 +5,9 @@ def exists():
     """ Check that stocktax.py exists """
     check50.exists("stocktax.py")
 
-# @check50.check(exists)
-# def run():
-#     """ File runs without syntax errors and greets the user correctly"""
-#     output = check50.run("python stocktax.py").stdin("single").stdin("100000").stdout()
-#     check50.log(output)
+@check50.check(exists)
+def run():
+    """ File runs without syntax errors and greets the user correctly"""
+    output = check50.run("python stocktax.py").stdin("single").stdin("100000").stdout()
+    check50.log(output)
 

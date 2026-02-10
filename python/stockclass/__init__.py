97291a7f40cd6e9fcb5db92cba64c0c41f463672
Marianne Bezaire <6456664+mbezaire@users.noreply.github.com>
2026-02-10 10:14:13 -0500

    add file

--- python/stockclass/__init__.py
+++ python/stockclass/__init__.py
@@ -15,5 +15,6 @@ def run():
 @check50.check(run)
 def stock():
     """ The stock class works alright"""
+    check50.include("makestock.py")
     output = check50.run("python makestock.py").stdout()
     check50.log(output)
